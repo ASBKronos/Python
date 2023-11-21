bc62e0eed6476053ee9a575590b828e9ab4fcba8
ASBKronos <133261437+ASBKronos@users.noreply.github.com>
2023-11-21 16:58:28 +0530

    Update Factorials Using Recursive Functions.py

--- Factorials Using Recursive Functions.py
+++ Factorials Using Recursive Functions.py
@@ -1,16 +1,16 @@
-def fibo(n):
+def fact(n):
     if n == 1 or n == 0:
         return 1
     elif n < 0:
         print("Enter a positive number")
     else:
-        return n * fibo(n-1)
+        return n * fact(n-1)
 
 
 while True:
 
     num = int(input("Enter the number who's factorial you require: "))
-    x = fibo(num)
+    x = fact(num)
     print(f"The factorial of {num} is : {x}")
     choice = str.lower(input("Do you want to enter another number (y/n)?"))[0]
 
